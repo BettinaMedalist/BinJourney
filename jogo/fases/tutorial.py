8469a68c76dfab8d5243e027b69fa0a5682fd157
Heitor Gomes <10heitor.gomes10@gmail.com>
2025-07-16 23:49:46 -0300

    tutorial

--- jogo/fases/tutorial.py
+++ jogo/fases/tutorial.py
@@ -4,18 +4,25 @@ from classes.tile import*
 
 # A matriz continua a mesma por enquanto, você a modificará depois com os novos códigos
 TUTORIAL_LAYOUT = [
-    'WWWWWWWWWWWWWWWWWWWWWWW',
+    'WWWWWWWWWWWWWWWWWWWWWWWW',
+    'W                      W',
+    'W          P           W',
     'W                      W',
     'W                      W',
     'W                      W',
-    'W             P        W',
+    'W                      W',
+    'W                      W',
+    'W                      W',
+    'W                      W',
+    'W                      W',
+    'W                      W',
+    'W                      W',
+    'W                      W',
+    'W                      W',
+    'W                      W',
+    'W         F            W',
+    'W        ASD           W',
     'W         J            W',
-    'W                      W',
-    'W                      W',
-    'W                      W',
-    'W                      W',
-    'W                      W',
-    'W                      W',
     'W                      W',
     'WWWWWWWWWWWWWWWWWWWWWWWW',
 ]
@@ -29,6 +36,10 @@ class Tutorial(Fase):
         # Carrega as imagens dos tiles
         self.chao_tile_img = pygame.image.load("jogo/sprites/chaoconcreto.png").convert_alpha()
         self.parede_tile_img = pygame.image.load("jogo/sprites/paredetuto.png").convert_alpha()
+        self.W_tile_img = pygame.image.load("jogo/sprites/W.png").convert_alpha()
+        self.A_tile_img = pygame.image.load("jogo/sprites/A.png").convert_alpha()
+        self.S_tile_img = pygame.image.load("jogo/sprites/S.png").convert_alpha()
+        self.D_tile_img = pygame.image.load("jogo/sprites/D.png").convert_alpha()
 
         # Loop que lê a matriz e constrói o nível
         for row_index, row in enumerate(TUTORIAL_LAYOUT):
@@ -63,3 +74,28 @@ class Tutorial(Fase):
                     # Teríamos que definir um ponto padrão aqui, ou criá-lo de forma diferente.
                     # Por enquanto, criamos ele sem um ponto B (ponto_b=None).
                     self.add_enemy("pistola_patrulha", 0, x, y, ponto_b=None)
+                elif tile_code == "F":
+                    Tile(self.W_tile_img,x,y, groups=[self.background_group])
+                elif tile_code == "A":
+                    Tile(self.A_tile_img,x,y, groups=[self.background_group])
+                elif tile_code == "S":
+                    Tile(self.S_tile_img,x,y, groups=[self.background_group])
+                elif tile_code == "D":
+                    Tile(self.D_tile_img,x,y, groups=[self.background_group])
+        screen_center_x = self.screen.get_rect().centerx
+        screen_center_y = self.screen.get_rect().centery
+
+        shift_x = self.player.rect.centerx - screen_center_x
+        shift_y = self.player.rect.centery - screen_center_y
+
+        for group in self.world_groups:
+            for sprite in group:
+                sprite.rect.x -= shift_x
+                sprite.rect.y -= shift_y
+        
+        for shot in self.player.shots:
+            shot.rect.x -= shift_x
+            shot.rect.y -= shift_y
+
+        self.player.rect.x -= shift_x
+        self.player.rect.y -= shift_y
